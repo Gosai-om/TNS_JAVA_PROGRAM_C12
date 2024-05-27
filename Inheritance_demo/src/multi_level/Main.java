08f9ae71aa9cb509be1190f8e8112ebbff5c26a9
Gosai-om <mca1.c.21@gmail.com>
2024-05-27 21:46:27 +0530

    day6_Inheritance_Demo

---
+++ Inheritance_demo/src/multi_level/Main.java
@@ -0,0 +1,16 @@
+package multi_level;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		City c = new City();
+		
+		c.setCnt_name("India");
+		c.setSname("Gujarat");
+		c.setCname("Ahmedabad");
+		
+		System.out.println(c);
+	}
+
+}
